8030fef4cee9974dda6ea5c44837adc45bd34833
Leonardo Isaias <leoisaias135@gmail.com>
2025-01-10 12:48:34 -0300

    Projeto finalizado utilizando todos os conceitos aprendidos

---
+++ src/br/com/alura/screenmatch/principal/Principal.java
@@ -0,0 +1,48 @@
+import br.com.alura.screenmatch.calculos.CalculadoraDeTempo;
+import br.com.alura.screenmatch.calculos.FiltroRecomendacao;
+import br.com.alura.screenmatch.modelos.Episodio;
+import br.com.alura.screenmatch.modelos.Filme;
+import br.com.alura.screenmatch.modelos.Serie;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Principal {
+
+    public static void main(String[] args) {
+
+        Filme favorito = new Filme("The Matrix", 1999);
+        favorito.setDuracaoEmMinutos(135);
+        favorito.setIncluidoNoPlano(true);
+
+        Filme outro = new Filme("John Wick", 2014);
+        outro.setAnoDeLancamento(2014);
+        outro.setDuracaoEmMinutos(101);
+        outro.setIncluidoNoPlano(true);
+
+        Serie serie = new Serie("La Casa de Papel", 2017);
+        serie.setIncluidoNoPlano(true);
+        serie.setAtiva(true);
+        serie.setTemporadas(5);
+        serie.setEpisodiosPorTemporada(10);
+        serie.setMinutosPorEpisodio(45);
+
+        CalculadoraDeTempo calculadora = new CalculadoraDeTempo();
+        calculadora.inclui(favorito);
+        calculadora.inclui(outro);
+        calculadora.inclui(serie);
+
+        System.out.println("Tempo total: " +calculadora.getTempoTotal());
+
+        Episodio primeiro = new Episodio();
+        primeiro.setNumero(1);
+        primeiro.setSerie(serie);
+        primeiro.setTotalVisualizacoes(300);
+
+        FiltroRecomendacao filtro = new FiltroRecomendacao();
+        filtro.filtrar(favorito);
+        filtro.filtrar(outro);
+        filtro.filtrar(primeiro);
+
+    }
+}
